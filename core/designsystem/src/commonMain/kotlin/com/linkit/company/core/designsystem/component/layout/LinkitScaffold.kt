b430e6f2f0d027ffb0716745ea3dd0ec9edb296b
Jaesung Lee <viki0114@naver.com>
2025-12-18 17:07:02 +0900

    LinkitScaffold 추가
    (cherry picked from commit f78a67626a897a81c167c612ca5e78267824e4ef)

---
+++ core/designsystem/src/commonMain/kotlin/com/linkit/company/core/designsystem/component/layout/LinkitScaffold.kt
@@ -0,0 +1,34 @@
+package com.linkit.company.core.designsystem.component.layout
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.linkit.company.core.designsystem.theme.LinkitTheme
+
+@Composable
+fun LinkitScaffold(
+    modifier: Modifier = Modifier,
+    containerColor: Color = LinkitTheme.color.gray100,
+    topBar: @Composable () -> Unit = {},
+    bottomBar: @Composable () -> Unit = {},
+    content: @Composable (PaddingValues) -> Unit,
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = topBar,
+        bottomBar = bottomBar,
+        containerColor = containerColor,
+    ) { paddingValues ->
+        // TODO : ContentMargin 적용 필요
+        val innerPadding = PaddingValues(
+            top = paddingValues.calculateTopPadding(),
+            start = 0.dp,
+            end = 0.dp,
+            bottom = paddingValues.calculateBottomPadding(),
+        )
+        content(innerPadding)
+    }
+}
