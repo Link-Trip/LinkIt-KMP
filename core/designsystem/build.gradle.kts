37dedd78df6e3d90d92a8396e79c2f48b93a3e9f
Jaesung Lee <viki0114@naver.com>
2025-12-18 15:39:59 +0900

    Color Token 정의
    (cherry picked from commit a4f52863c5112ee29a5b089b7a47b73e9128ddb0)

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -3,9 +3,7 @@ plugins {
     id("kmp.screenshot.test.convention")
 }
 
-android {
-    namespace = "com.linkit.company.core.designsystem"
-}
+android.namespace = "com.linkit.company.core.designsystem"
 
 val composeExtension = extensions.getByType(org.jetbrains.compose.ComposeExtension::class.java)
 
@@ -15,4 +13,10 @@ kotlin {
             implementation(composeExtension.dependencies.materialIconsExtended)
         }
     }
+}
+
+compose.resources {
+    publicResClass = true
+    // ref. https://www.jetbrains.com/help/kotlin-multiplatform-dev/whats-new-compose-180.html#option-to-change-the-generated-res-class-name
+    nameOfResClass = "DesignRes"
 }
